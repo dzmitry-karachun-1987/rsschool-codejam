82456b43c32ad9823e4ac573a3178553f0d0bc67
Dzmitry Karachun <dzmitry.karachun@bk.ru>
2018-10-28 11:55:57 +0300

    fix sumOfOther.js

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,11 +1,11 @@
 function sumOfOther(array) {
-    let newArray = [];
+    let resArray = [];
     let sum = 0;
     for(let i = 0; i < array.length; i++) {
         sum = sum + array[i];	
     }
     for(let i = 0; i < array.length; i++) { 
-        newArray[i] = sum - array[i];
+        resArray[i] = sum - array[i];
     }
-    return newArray;
+    return resArray;
   }
